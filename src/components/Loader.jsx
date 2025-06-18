7d24da1e022f75e9b228f6bccdecf50e3f335b47
Saumy-TOXOTIS <saumytiwari95@gmail.com>
2025-06-18 08:36:51 +0530

    Loader Enhancement

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,15 +1,19 @@
 // src/components/Loader.jsx
-
 import React from 'react';
 
 export default function Loader() {
     return (
         <div className="loader-container">
-            <div className="loader-content">
-                <div className="spinner"></div>
-                <h1>Initializing 3D Experience...</h1>
-                <p>Please wait, awesome things are loading!</p>
+            <div className="loader-grid">
+                {[...Array(16)].map((_, i) => (
+                    <div key={i} className="grid-cell" style={{ '--delay': i * 0.05 }}></div>
+                ))}
             </div>
+            <div className="loader-text">
+                <span className="loader-title">LOADING</span>
+                <span className="loader-subtitle">Initializing 3D Experience</span>
+            </div>
+            <div className="scanline"></div>
         </div>
     );
 }
